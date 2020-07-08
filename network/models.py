7b1e1455a7040758033fe8d33665a3135ba7cb02
Asential <Asential@users.noreply.github.com>
2020-07-08 15:25:43 +0530

    Update - 1
    Added Models
    Added Post CSS

--- network/models.py
+++ network/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=400)
+    likes = models.IntegerField(default=0)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    time = models.TimeField(auto_now=False, auto_now_add=True)
+    def __str__(self):
+        return f"{self.user} : {self.content}"
--- network/views.py
+++ network/views.py
@@ -4,11 +4,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Post
 
 
 def index(request):
-    return render(request, "network/index.html")
+
+    posts = Post.objects.all().order_by('-id')
+    return render(request, "network/index.html", {
+        "posts": posts
+    })
 
 
 def login_view(request):
